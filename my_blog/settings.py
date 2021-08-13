0df23fbb96b84b5d97bbee52748bedb5fa368632
AndreyShekhovtsov <shekhovtsovandrey@mail.ru>
2021-08-14 02:08:45 +0300

    add db mysql

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_blog',
+        'USER': 'root',
+        'PASSWORD': '19712703Nata',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
