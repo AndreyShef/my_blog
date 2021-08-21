6a4a91dcdea207fba1db589a101ee9638c47187b
AndreyShekhovtsov <shekhovtsovandrey@mail.ru>
2021-08-21 12:57:41 +0300

    telegram bot ver1

--- crm/views.py
+++ crm/views.py
@@ -3,6 +3,8 @@ from .models import Order
 from .forms import OrderForm
 from cms.models import CmsSlider
 from price.models import PriceCard, PriceTable
+from telebot.sendmessage import sendTelegram
+
 
 # Create your views here.
 def first_page(request):
@@ -11,12 +13,14 @@ def first_page(request):
     pc_2 = PriceCard.objects.get(pk=2)
     pc_3 = PriceCard.objects.get(pk=3)
     price_table = PriceTable.objects.all()
+    form = OrderForm()
     dict_obj = {
         'slider_list': slider_list,
         'pc_1': pc_1,
         'pc_2': pc_2,
         'pc_3': pc_3,
         'price_table': price_table,
+        'form': form,
     }
     return render(request, './index.html', dict_obj)
 
@@ -25,7 +29,8 @@ def thanks_page(request):
     phone = request.POST['phone']
     element = Order(order_name=name, order_phone=phone)
     element.save()
-    return render(request, './thanks_page.html', {
+    sendTelegram()
+    return render(request, './thanks.html', {
         'name': name,
         'phone': phone
     })
