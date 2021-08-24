6cf84754285c70bfd4ef5f08b99463ee68b794ae
AndreyShekhovtsov <shekhovtsovandrey@mail.ru>
2021-08-24 19:24:57 +0300

    final version of blog

--- telebot/sendmessage.py
+++ telebot/sendmessage.py
@@ -2,15 +2,37 @@ import requests
 from .models import TeleSettings
 
 
-def sendTelegram():
-    settings = TeleSettings.objects.get(pk=1)
-    token = str(settings.tg_token)
-    chat_id = str(settings.tg_chat)
-    text = str(settings.tg_message)
-    api = 'https://api.telegram.org/bot'
-    method = api + token + '/sendMessage'
+def sendTelegram(tg_name, tg_phone):
+    if TeleSettings.objects.get(pk=1):
+        settings = TeleSettings.objects.get(pk=1)
+        token = str(settings.tg_token)
+        chat_id = str(settings.tg_chat)
+        text = str(settings.tg_message)
+        api = 'https://api.telegram.org/bot'
+        method = api + token + '/sendMessage'
 
-    req = requests.post(method, data={
-        'chat_id': chat_id,
-        'text': text,
-    })
+        if text.find(':') and text.rfind(':'):
+            prt1 = text[0:text.find(':')+1]
+            prt2 = text[text.find(':')+1:text.rfind(':')+1]
+
+            text_slice = prt1 + ' ' + tg_name + prt2 + ' ' + tg_phone
+        else:
+            text_slice = text
+
+        try:
+            req = requests.post(method, data={
+                'chat_id': chat_id,
+                'text': text_slice,
+            })
+        except:
+            pass
+        finally:
+            if req.status_code != 200:
+                print('Ошибка отправки!')
+            elif req.status_code == 500:
+                print('Ошибка 500')
+            else:
+                print('Сообщение успешно отправлено!')
+
+    else:
+        pass
